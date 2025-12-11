a1267a2684163f3a92a4b7f27377ce093c61bcb5
ALssW <alsws@qq.com>
2025-12-11 23:46:20 +0800

    03_常量

---
+++ 03_常量.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// 宏常量
+#define Day 7
+
+
+int main_3()
+{
+	// Day = 14  错误，常量不可修改
+	cout << "一周有 " << Day << " 天。" << endl;
+
+	const int month = 12;
+
+	cout << "一年有 " << month << " 个月。" << endl;
+	
+	return 0;
+}
